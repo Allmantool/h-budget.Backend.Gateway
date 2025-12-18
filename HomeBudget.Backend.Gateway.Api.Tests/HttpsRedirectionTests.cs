c10bf358f762f5db378a7d34c97beccecd3ed4f0
ROOT-LAPTOP\pavel <pavel.chyzhykau@gmail.com>
2025-12-18 09:46:16 +0300

    tech: add tests

---
+++ HomeBudget.Backend.Gateway.Api.Tests/HttpsRedirectionTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc.Testing;
+
+using HomeBudget.Backend.Gateway.Constants;
+
+namespace HomeBudget.Backend.Gateway.Api.Tests
+{
+    [TestFixture]
+    public class HttpsRedirectionTests
+    {
+        private GatewayWebApplicationFactory _factory = null!;
+        private HttpClient _client = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            _factory = new GatewayWebApplicationFactory();
+
+            _client = _factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _client.Dispose();
+            _factory.Dispose();
+        }
+
+        [Test]
+        public async Task Http_Request_To_Regular_Endpoint_Should_Redirect_To_Https()
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Get, "/swagger");
+
+            var response = await _client.SendAsync(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Redirect));
+
+            var location = response.Headers.Location;
+            Assert.That(location, Is.Not.Null);
+            Assert.That(location!.Scheme, Is.EqualTo("https"));
+        }
+
+        [TestCase(Endpoints.HealthCheckSource)]
+        [TestCase(Endpoints.Metrics)]
+        public async Task Http_Request_To_Internal_Endpoints_Should_Not_Redirect(string path)
+        {
+            using var response = await _client.GetAsync(path);
+
+            Assert.That(response.StatusCode, Is.Not.EqualTo(HttpStatusCode.Redirect));
+            Assert.That(response.StatusCode, Is.Not.EqualTo(HttpStatusCode.MovedPermanently));
+        }
+    }
+}
