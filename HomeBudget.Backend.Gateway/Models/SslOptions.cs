996d5390e2944f36fe1c9ff585b97e3cf75e6f9b
ROOT-LAPTOP\pavel <pavel.chyzhykau@gmail.com>
2026-01-02 01:20:11 +0300

    feat: adjust hptts cert setup

--- HomeBudget.Backend.Gateway/Models/SslOptions.cs
+++ HomeBudget.Backend.Gateway/Models/SslOptions.cs
@@ -1,10 +1,15 @@
-﻿namespace HomeBudget.Backend.Gateway.Models
+﻿using System.IO;
+
+namespace HomeBudget.Backend.Gateway.Models
 {
-    public record SslOptions
+    internal record SslOptions
     {
         public int HttpsPort { get; set; }
         public int HttpPort { get; set; }
+        public string CertificatePath { get; set; } = "/app";
         public string CertificateName { get; set; }
         public string Password { get; set; }
+
+        public string GetFullPath() => Path.Combine(CertificatePath, CertificateName);
     }
 }
